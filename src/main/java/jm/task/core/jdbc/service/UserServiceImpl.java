5b9e822fef688df5732b7985d9d64d39a11c9cdc
m4sbobkov <alexbob14@mail.ru>
2022-08-21 19:47:13 +0400

    1.1.4

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
@@ -8,7 +9,7 @@ import java.sql.Statement;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDaoJDBCImpl dao = new UserDaoJDBCImpl();
+    private UserDaoHibernateImpl dao = new UserDaoHibernateImpl();
     public UserServiceImpl() {
     }
 
